2e6634fe501f8975890b06a26a8a230a127f409b
Wilie12 <albertw536@gmail.com>
2025-10-03 22:38:53 +0200

    Basic repository set up with test

---
+++ src/main/java/com/nn/spring_blog_rest_api/article/domain/Article.java
@@ -0,0 +1,89 @@
+package com.nn.spring_blog_rest_api.article.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "articles")
+public class Article {
+    @Id
+    @GeneratedValue
+    private long id;
+    private int version;
+    private String title;
+    private String content;
+    private final List<String> tags = new ArrayList<>();
+    private ZonedDateTime publishedDate;
+    private ZonedDateTime updatedDate;
+
+    protected Article() {}
+
+    public Article(String title, String content, List<String> tags) {
+        version = 1;
+        this.title = title;
+        this.content = content;
+        publishedDate = ZonedDateTime.now(ZoneId.of("UTC"));
+        updatedDate = ZonedDateTime.now(ZoneId.of("UTC"));
+
+        for (String tag : tags) {
+            addTag(tag);
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void updateVersion() {
+        version++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+
+    public ZonedDateTime getPublishedDate() {
+        return publishedDate;
+    }
+
+    public ZonedDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(ZonedDateTime updatedDate) {
+        this.updatedDate = ZonedDateTime.now(ZoneId.of("UTC"));
+    }
+}
